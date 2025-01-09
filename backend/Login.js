986ab0844dba01993a55a3d507c5153c1877bc1d
yash raj mishra <yashraj06112001@gmail.com>
2025-01-09 23:19:22 +0530

    completed the work related to the login of the page

--- backend/Login.js
+++ backend/Login.js
@@ -6,23 +6,27 @@ const connection = require("./databse");
 const bycrypt = require("bycrypt");
 
 const loginRecord = router.post("/login", async (req, res) => {
-  const { customerId, password, dateAndTime } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const sqlQuery = `INSERT INTO login (customerId, Password, dateAndTime) VALUES (?, ?, ?)`;
-  connection.query(
-    sqlQuery,
-    [customerId, hashedPassword, dateAndTime],
-    (error, results) => {
-      if (error) {
-        console.error("Error inserting data:", error);
-        return res.status(500).json({ message: "Database error" });
+  try {
+    const { customerId, password, dateAndTime } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const sqlQuery = `INSERT INTO login (customerId, Password, dateAndTime) VALUES (?, ?, ?)`;
+    connection.query(
+      sqlQuery,
+      [customerId, hashedPassword, dateAndTime],
+      (error, results) => {
+        if (error) {
+          console.error("Error inserting data:", error);
+          return res.status(500).json({ message: "Database error" });
+        }
+        res.status(201).json({
+          message: "Login record inserted successfully",
+          recordId: results.insertId,
+        });
       }
-      res.status(201).json({
-        message: "Login record inserted successfully",
-        recordId: results.insertId,
-      });
-    }
-  );
+    );
+  } catch (err) {
+    console.log("we are facing error in login the values");
+  }
 });
 
 module.exports = {
